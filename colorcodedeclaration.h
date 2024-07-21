1bbdba5007b0ad9312979980a50d9ed6d9d01304
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-21 16:50:25 +0530

    Update colorcodedeclaration.h

--- colorcodedeclaration.h
+++ colorcodedeclaration.h
@@ -23,12 +23,13 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
+/*
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
         MinorColorNames[colorPair->minorColor]);
 }
-/*
+
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
