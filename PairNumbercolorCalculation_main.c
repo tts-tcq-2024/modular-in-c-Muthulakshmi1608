8d1ebf6b5e745233d702ec99712b279207f4ac92
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-22 15:25:07 +0530

    Update PairNumbercolorCalculation_main.c

--- PairNumbercolorCalculation_main.c
+++ PairNumbercolorCalculation_main.c
@@ -1,18 +1,18 @@
 #include "colorcodedeclaration.h"
 #define MAX_COLORPAIR_NAME_CHARS 16
-bool Numbertocolor_flg;
+
 void testNumberToPair(int pairNumber,
     enum MajorColor expectedMajor,
     enum MinorColor expectedMinor)
 {
-    Numbertocolor_flg=1;
+    
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
     ColorPairToString(&colorPair, colorPairNames);
     printf("Got pair %s\n", colorPairNames);
     assert(colorPair.majorColor == expectedMajor);
     assert(colorPair.minorColor == expectedMinor);
-    printreferencemanual(&colorPair,pairNumber,Numbertocolor_flg);
+  
 }
 
 
@@ -21,12 +21,12 @@ void testPairToNumber(
     enum MinorColor minor,
     int expectedPairNumber)
 {
-    Numbertocolor_flg=0;
+   
     ColorPair colorPair;
     colorPair.majorColor = major;
     colorPair.minorColor = minor;
     int pairNumber = GetPairNumberFromColor(&colorPair);
     printf("Got pair number %d\n", pairNumber);
     assert(pairNumber == expectedPairNumber);
-    printreferencemanual(&colorPair,pairNumber,Numbertocolor_flg);
+   
 }
