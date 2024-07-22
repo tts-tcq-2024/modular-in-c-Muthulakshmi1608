88b58ed83b7e8fa36d90895caff304fc169d8baa
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-22 15:22:02 +0530

    Update colorcodecalculationtests.c

--- colorcodecalculationtests.c
+++ colorcodecalculationtests.c
@@ -1,16 +1,4 @@
 #include "colorcodedeclaration.h"
-void printreferencemanual(const ColorPair* colorPair,int pairNumber,bool Numbertocolor_flg)
-{
-    if(Numbertocolor_flg)
-    {
-        printf("Pair No: %d, Major color: %s,Minor Color: %s\n",pairNumber,(MajorColorNames[colorPair->majorColor]),(MinorColorNames[colorPair->minorColor]));
-    }
-    else
-    {    
-        printf("Pair No: %d, Major color: %s,Minor Color: %s\n",pairNumber,(MajorColorNames[colorPair->majorColor]),(MinorColorNames[colorPair->minorColor]));
-    }
-}
-
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
