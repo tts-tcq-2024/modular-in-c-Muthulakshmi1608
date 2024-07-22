3d99a7e788e36b057d70d4ee0ecfb54237e00ed1
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-22 15:23:55 +0530

    Update PairNumberColorCalculation.c

--- PairNumberColorCalculation.c
+++ PairNumberColorCalculation.c
@@ -33,3 +33,11 @@ int GetPairNumberFromColor(const ColorPair* colorPair) {
             colorPair->minorColor + 1;
 }
 
+void PrintColorCodingReferenceManual() {
+    char colorPairNames[16];
+    for (int pairNumber = 1; pairNumber <= numberOfMajorColors * numberOfMinorColors; pairNumber++) {
+        ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+        ColorPairToString(&colorPair, colorPairNames);
+        printf("%d: %s\n", pairNumber, colorPairNames);
+    }
+}
