b26efe2141eac007e6e9c7a7aa3321d26423bd21
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-27 20:53:52 +0530

    Create printcolor_coding_referencemanual.h

---
+++ printcolor_coding_referencemanual.h
@@ -0,0 +1,9 @@
+#ifndef PRINTCOLOR_CODING_REFERENCEMANUAL_H
+#define PRINTCOLOR_CODING_REFERENCEMANUAL_H
+
+#include "convert_colorpair_to_string.h"
+#include "identify_color_from_pairnumber.h"
+
+void PrintColorCodingReferenceManual();
+
+#endif
