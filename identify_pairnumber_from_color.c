ab8b588f51d112e9e1aa8279c8786390aea6f8d2
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-27 19:55:48 +0530

    Create identify_pairnumber_from_color.c

---
+++ identify_pairnumber_from_color.c
@@ -0,0 +1,6 @@
+#include "identify_pairnumber_from_color.h"
+
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
