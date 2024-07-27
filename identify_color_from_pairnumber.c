d6219bcb20d9ec8abb619b1f9ad50f80a55e80f3
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-27 19:55:20 +0530

    Create identify_color_from_pairnumber.c

---
+++ identify_color_from_pairnumber.c
@@ -0,0 +1,11 @@
+#include "identify_color_from_pairnumber.h"
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
