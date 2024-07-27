335b90ddff2bafd6bb12e377772fc8add02c1bc5
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-27 20:54:04 +0530

    Update color_code_initialization.c

--- color_code_initialization.c
+++ color_code_initialization.c
@@ -16,11 +16,4 @@ int numberOfMinorColors =
 
 
 
-void PrintColorCodingReferenceManual() {
-    char colorPairNames[16];
-    for (int pairNumber = 1; pairNumber <= numberOfMajorColors * numberOfMinorColors; pairNumber++) {
-        ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-        ColorPairToString(&colorPair, colorPairNames);
-        printf("%d: %s\n", pairNumber, colorPairNames);
-    }
-}
+
